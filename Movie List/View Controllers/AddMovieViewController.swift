3812e7d1146ed1111c8598c003e9e7f68ca75318
Peggy Wollenhaupt <isande@gmail.com>
2020-05-30 12:30:11 -0600

    MVP complete

---
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Peggy Wollenhaupt on 5/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+protocol AddMovieDelegate {
+    func movieWasAdded(_ movie: Movie)
+}
+
+class AddMovieViewController: UIViewController {
+
+    @IBOutlet weak var addMovieTextField: UITextField!
+    
+    var delegate: AddMovieDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addMovieTapped(_ sender: Any) {
+        guard let title = addMovieTextField.text else { return }
+        
+        let movie = Movie(name: title)
+        delegate?.movieWasAdded(movie)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
